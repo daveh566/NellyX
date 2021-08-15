2256c82a7eb4b9c5d0ee0d273ff82952d0ad5254
daveh566 <69384676+daveh566@users.noreply.github.com>
2021-08-15 12:16:45 +0300

    Update Nelly.py

--- Nelly.py
+++ Nelly.py
@@ -1,9 +1,10 @@
 print("[INFO]: Importing Your API_ID, API_HASH, BOT_TOKEN")
 import re
 from asyncio import (gather, get_event_loop, sleep)
-
+from pyrogram import filters
 from aiohttp import ClientSession
 from pyrogram import (Client, filters, idle)
+from Python_ARQ import ARQ
 
 from config import bot, BOT_TOKEN, ARQ_API_KEY, ARQ_API_BASE_URL, LANGUAGE
 bot_token= BOT_TOKEN
@@ -12,7 +13,45 @@ print("[INFO]: Checking... Your Details")
 
 bot_id = int(bot_token.split(":")[0])
 print("[INFO]: Code running by master Prince Op")
- message._client.send_chat_action(chat_id, "cancel")
+arq = None
+
+
+async def lunaQuery(query: str, user_id: int):
+    query = (
+        query
+        if LANGUAGE == "en"
+        else (await arq.translate(query, "en")).result.translatedText
+    )
+    resp = (await arq.luna(query, user_id)).result
+    return (
+        resp
+        if LANGUAGE == "en"
+        else (
+            await arq.translate(resp, LANGUAGE)
+        ).result.translatedText
+    )
+
+
+async def type_and_send(message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id if message.from_user else 0
+    query = message.text.strip()
+    await message._client.send_chat_action(chat_id, "typing")
+    response, _ = await gather(lunaQuery(query, user_id), sleep(2))
+    if "Luna" in response:
+        responsee = response.replace("Luna", "Nelly")
+    else:
+        responsee = response
+    if "Aco" in responsee:
+        responsess = responsee.replace("Aco", "Nelly")
+    else:
+        responsess = responsee
+    if "Who is Nelly?" in responsess:
+        responsess2 = responsess.replace("Who is Tiana?", "Heroine Of Telegram")
+    else:
+        responsess2 = responsess
+    await message.reply_text(responsess2)
+    await message._client.send_chat_action(chat_id, "cancel")
 
 
 @bot.on_message(
@@ -54,15 +93,18 @@ async def chatpm(_, message):
 
 @bot.on_message(filters.command("start") & ~filters.edited)
 async def startt(_, message):
-    await message.reply_text("Hi, I'm Alive ╮(. ❛ ᴗ ❛.)╭")
+    await message.reply_text("Hi, I'm Nelly My master is @aspirer for repo join @kayaspirerproject")
 
 
 async def main():
+    global arq
     session = ClientSession()
-   await bot.start()
+    arq = ARQ(ARQ_API_BASE_URL, ARQ_API_KEY, session)
+
+    await bot.start()
     print(
         """
-Your bot Is Deployed Successfully.
+Nelly By Aspirer Is Deployed Successfully.
 """
     )
     await idle()
