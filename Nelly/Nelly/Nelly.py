51b0484fd500cff58cedbc52072cb74b7465b737
daveh566 <69384676+daveh566@users.noreply.github.com>
2021-08-24 17:59:04 +0300

    Create Nelly.py

---
+++ Nelly/Nelly/Nelly.py
@@ -0,0 +1,284 @@
+import asyncio
+import aiohttp
+import emoji
+import requests
+import re
+from lycia import NELLY
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client, filters
+from google_trans_new import google_translator
+url = "https://acobot-brainshop-ai-v1.p.rapidapi.com/get"
+
+translator = google_translator()
+
+BOT_ID = 1688991183
+
+def extract_emojis(s):
+    return "".join(c for c in s if c in emoji.UNICODE_EMOJI)
+
+#Chatbot Modules By  @aspirer2
+
+en_chats = []
+
+@NELLY.on_message(
+    filters.text & filters.reply & ~filters.bot & ~filters.via_bot & ~filters.forwarded,
+    group=2,
+)
+async def nelly(client, message):
+    if message.reply_to_message.from_user.id != BOT_ID:
+        message.continue_propagation()
+    msg = message.text
+    chat_id = message.chat.id
+    if msg.startswith("/") or msg.startswith("@"):
+        message.continue_propagation()
+    if chat_id in en_chats:
+        aura = msg
+        aura = aura.replace("nelly", "Aco")
+        aura = aura.replace("Nelly", "Aco")
+        querystring = {
+            "bid": "178",
+            "key": "sX5A2PcYZbsN5EY6",
+            "uid": "mashape",
+            "msg": {test},
+        }
+        headers = {
+            "x-rapidapi-key": "cf9e67ea99mshecc7e1ddb8e93d1p1b9e04jsn3f1bb9103c3f",
+            "x-rapidapi-host": "acobot-brainshop-ai-v1.p.rapidapi.com",
+        }
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        result = response.text
+        result = result.replace('{"cnt":"', "")
+        result = result.replace('"}', "")
+        result = result.replace("Aco", "Nelly")
+        result = result.replace("Eliza", "Nelly")
+        result = result.replace("Hi~", "Hello Friend I Am Nelly")
+        result = result.replace("My dear great botmaster, Nellybot Team.", "Made By @madepranav")
+        result = result.replace("Have the control right.", "My Father Is @madepranav")
+        result = result.replace("I was created by NellyBot Team @kayaspirerproject.", "I was created by @Techno_Ocean Team.")
+        result = result.replace("<a href=\\", "<a href =")
+        result = result.replace("<\/a>", "</a>")
+        red = result
+        try:
+            await NELLY.send_chat_action(message.chat.id, "typing")
+            await message.reply_text(red)
+        except CFError as e:
+            print(e)
+    else:
+        u = msg.split()
+        emj = extract_emojis(msg)
+        msg = msg.replace(emj, "")
+        if (
+            [(k) for k in u if k.startswith("@")]
+            and [(k) for k in u if k.startswith("#")]
+            and [(k) for k in u if k.startswith("/")]
+            and re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []
+        ):
+
+            h = " ".join(filter(lambda x: x[0] != "@", u))
+            km = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", h)
+            tm = km.split()
+            jm = " ".join(filter(lambda x: x[0] != "#", tm))
+            hm = jm.split()
+            rm = " ".join(filter(lambda x: x[0] != "/", hm))
+        elif [(k) for k in u if k.startswith("@")]:
+
+            rm = " ".join(filter(lambda x: x[0] != "@", u))
+        elif [(k) for k in u if k.startswith("#")]:
+            rm = " ".join(filter(lambda x: x[0] != "#", u))
+        elif [(k) for k in u if k.startswith("/")]:
+            rm = " ".join(filter(lambda x: x[0] != "/", u))
+        elif re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []:
+            rm = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", msg)
+        else:
+            rm = msg
+            lan = translator.detect(rm)
+        aura = rm
+        if not "en" in lan and not lan == "":
+            aura = translator.translate(aura, lang_tgt="en")
+
+        aura = aura.replace("nelly", "Aco")
+        aura = aura.replace("Nelly", "Aco")
+        querystring = {
+            "bid": "178",
+            "key": "sX5A2PcYZbsN5EY6",
+            "uid": "mashape",
+            "msg": {aura},
+        }
+        headers = {
+            "x-rapidapi-key": "cf9e67ea99mshecc7e1ddb8e93d1p1b9e04jsn3f1bb9103c3f",
+            "x-rapidapi-host": "acobot-brainshop-ai-v1.p.rapidapi.com",
+        }
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        result = response.text
+        result = result.replace('{"cnt":"', "")
+        result = result.replace('"}', "")
+        result = result.replace("Aco", "Nelly")
+        result = result.replace("Eliza", "Nelly")
+        result = result.replace("Hi~", "Hello Friend I Am @Lyciachatbot")
+        result = result.replace("My dear great botmaster, Nellybot Team.", "Made By @madepranav")
+        result = result.replace("Have the control right.", "My Father Is @madepranav")
+        result = result.replace("I was created by  NellyBot Team @kayaspirerproject.", "I was created by @Techno_Ocean Team.")
+        result = result.replace("<a href=\\", "<a href =")
+        result = result.replace("<\/a>", "</a>")
+        red = result
+        if not "en" in lan and not lan == "":
+            pro = translator.translate(red, lang_tgt=lan[0])
+        try:
+            await NELLY.send_chat_action(message.chat.id, "typing")
+            await message.reply_text(red)
+        except CFError as e:
+            print(e)
+
+
+
+@NELLY.on_message(filters.text & filters.private & ~filters.reply & ~filters.bot)
+async def redaura(client, message):
+    msg = message.text
+    if msg.startswith("/") or msg.startswith("@"):
+        message.continue_propagation()
+    u = msg.split()
+    emj = extract_emojis(msg)
+    msg = msg.replace(emj, "")
+    if (
+        [(k) for k in u if k.startswith("@")]
+        and [(k) for k in u if k.startswith("#")]
+        and [(k) for k in u if k.startswith("/")]
+        and re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []
+    ):
+
+        h = " ".join(filter(lambda x: x[0] != "@", u))
+        km = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", h)
+        tm = km.split()
+        jm = " ".join(filter(lambda x: x[0] != "#", tm))
+        hm = jm.split()
+        rm = " ".join(filter(lambda x: x[0] != "/", hm))
+    elif [(k) for k in u if k.startswith("@")]:
+
+        rm = " ".join(filter(lambda x: x[0] != "@", u))
+    elif [(k) for k in u if k.startswith("#")]:
+        rm = " ".join(filter(lambda x: x[0] != "#", u))
+    elif [(k) for k in u if k.startswith("/")]:
+        rm = " ".join(filter(lambda x: x[0] != "/", u))
+    elif re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []:
+        rm = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", msg)
+    else:
+        rm = msg
+        lan = translator.detect(rm)
+    aura = rm
+    if not "en" in lan and not lan == "":
+        aura = translator.translate(aura, lang_tgt="en")
+
+   
+    aura = aura.replace("Nelly", "Aco")
+    aura = aura.replace("Nelly", "Aco")
+    querystring = {
+        "bid": "178",
+        "key": "sX5A2PcYZbsN5EY6",
+        "uid": "mashape",
+        "msg": {aura},
+    }
+    headers = {
+        "x-rapidapi-key": "cf9e67ea99mshecc7e1ddb8e93d1p1b9e04jsn3f1bb9103c3f",
+        "x-rapidapi-host": "acobot-brainshop-ai-v1.p.rapidapi.com",
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    result = response.text
+    result = result.replace('{"cnt":"', "")
+    result = result.replace('"}', "")
+    result = result.replace("Aco", "Lycia")
+    result = result.replace("Eliza", "@Lyciachatbot")
+    result = result.replace("Hi~", "Hello Friend I Am @Lyciachatbot")
+    result = result.replace("My dear great botmaster, Lyciabot Team.", "Made By @madepranav")
+    result = result.replace("Have the control right.", "My Father Is @madepranav")
+    result = result.replace("I was created by Lyciabot Team.", "I was created by @Techno_Ocean Team.")
+    result = result.replace("<a href=\\", "<a href =")
+    result = result.replace("<\/a>", "</a>")
+    red = result
+    if not "en" in lan and not lan == "":
+        red = translator.translate(red, lang_tgt=lan[0])
+    try:
+        await NELLY.send_chat_action(message.chat.id, "typing")
+        await message.reply_text(red)
+    except CFError as e:
+        print(e)
+
+
+@NELLY.on_message(
+    filters.regex("Nelly|nelly|NELLY")
+    & ~filters.bot
+    & ~filters.via_bot
+    & ~filters.forwarded
+    & ~filters.reply
+    & ~filters.channel
+)
+async def redaura(client, message):
+    msg = message.text
+    if msg.startswith("/") or msg.startswith("@"):
+        message.continue_propagation()
+    u = msg.split()
+    emj = extract_emojis(msg)
+    msg = msg.replace(emj, "")
+    if (
+        [(k) for k in u if k.startswith("@")]
+        and [(k) for k in u if k.startswith("#")]
+        and [(k) for k in u if k.startswith("/")]
+        and re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []
+    ):
+
+        h = " ".join(filter(lambda x: x[0] != "@", u))
+        km = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", h)
+        tm = km.split()
+        jm = " ".join(filter(lambda x: x[0] != "#", tm))
+        hm = jm.split()
+        rm = " ".join(filter(lambda x: x[0] != "/", hm))
+    elif [(k) for k in u if k.startswith("@")]:
+
+        rm = " ".join(filter(lambda x: x[0] != "@", u))
+    elif [(k) for k in u if k.startswith("#")]:
+        rm = " ".join(filter(lambda x: x[0] != "#", u))
+    elif [(k) for k in u if k.startswith("/")]:
+        rm = " ".join(filter(lambda x: x[0] != "/", u))
+    elif re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []:
+        rm = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", msg)
+    else:
+        rm = msg
+        lan = translator.detect(rm)
+    aura = rm
+    if not "en" in lan and not lan == "":
+        aura = translator.translate(aura, lang_tgt="en")
+
+
+    aura = aura.replace("Nelly", "Aco")
+    aura = aura.replace("Nelly", "Aco")
+    querystring = {
+        "bid": "178",
+        "key": "sX5A2PcYZbsN5EY6",
+        "uid": "mashape",
+        "msg": {aura},
+    }
+    headers = {
+        "x-rapidapi-key": "cf9e67ea99mshecc7e1ddb8e93d1p1b9e04jsn3f1bb9103c3f",
+        "x-rapidapi-host": "acobot-brainshop-ai-v1.p.rapidapi.com",
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    result = response.text
+    result = result.replace('{"cnt":"', "")
+    result = result.replace('"}', "")
+    result = result.replace("Aco", "Lycia")
+    result = result.replace("Eliza", "Nelly")
+    result = result.replace("Hi~", "Hello Friend I Am @elianachatbot")
+    result = result.replace("My dear great botmaster, Nellybot Team.", "Made By @madepranav")
+    result = result.replace("Have the control right.", "My Father Is @madepranav")
+    result = result.replace("I was created by NellyBot Team @Kayaspirerproject.", "I was created by @Techno_Ocean Team.")
+    result = result.replace("<a href=\\", "<a href =")
+    result = result.replace("<\/a>", "</a>")
+    pro = result
+    if not "en" in lan and not lan == "":
+        red = translator.translate(red, lang_tgt=lan[0])
+    try:
+        await NELLY.send_chat_action(message.chat.id, "typing")
+        await message.reply_text(red)
+    except CFError as e:
+        print(e)
+        
+       
