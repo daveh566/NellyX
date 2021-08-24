69647847908c12be47bb37e15e3ca90997179f68
daveh566 <69384676+daveh566@users.noreply.github.com>
2021-08-24 21:05:38 +0300

    Update config.py

--- Nelly/config.py
+++ Nelly/config.py
@@ -6,8 +6,3 @@ from pyrogram import Client
 API_ID = int(os.environ.get("API_ID", "6"))
 API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-
-NELLY = Client(":memory:",
-             api_id=API_ID,
-             api_hash=API_HASH,
-             bot_token=BOT_TOKEN
